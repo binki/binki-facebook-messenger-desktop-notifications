e753b05659fe4285cdb09bb7781b6c44437e94a0
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2022-03-03 10:02:47 -0500

    1.8

--- binki-facebook-messenger-desktop-notifications.user.js
+++ binki-facebook-messenger-desktop-notifications.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     binki-facebook-messenger-desktop-notifications
-// @version  1.7
+// @version  1.8
 // @grant    none
 // @author   Nathan Phillip Brink (binki) (@ohnobinki)
 // @homepageURL https://github.com/binki/binki-facebook-messenger-desktop-notifications/
